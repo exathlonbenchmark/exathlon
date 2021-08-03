fde03427db4be6abbe995c428bf1329ba818d724
Exathlon <exathlon.benchmark@gmail.com>
2021-08-03 22:08:30 +0200

    Fix and update extraction of large files

--- extract_data.sh
+++ extract_data.sh
@@ -5,11 +5,13 @@ rm "./data/raw/ground_truth.zip"
 for (( i=1; i<=10; i++ )); do
   DIR="./data/raw/app$i"
   unzip "$DIR/*.zip" -d $DIR
-  find $DIR -name "*.zip" -type f -delete
+  find $DIR -maxdepth 1 -type f -name "*.zip" -delete
   for subdir in $(find $DIR -mindepth 1 -type d); do
     ZIP_FILE_PATH=$(find $subdir -iname \*.zip)
-    zip -F $ZIP_FILE_PATH --out $DIR/$(basename $ZIP_FILE_PATH)
-    unzip $DIR/$(basename $ZIP_FILE_PATH) -d $DIR
+    ZIP_FILE_NAME=${ZIP_FILE_PATH##*/}
+    zip -s0 $ZIP_FILE_PATH --out $DIR/$ZIP_FILE_NAME
+    unzip $DIR/$ZIP_FILE_NAME -d $DIR
+    rm $DIR/$ZIP_FILE_NAME
     rm -rf $subdir
   done
 done
