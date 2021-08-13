4e0b75b13d3e39763d9e4d52e3c703f5f617e342
Exathlon <exathlon.benchmark@gmail.com>
2021-08-13 13:14:46 +0200

    Fix extraction issues for some large files

--- extract_data.sh
+++ extract_data.sh
@@ -9,7 +9,9 @@ for (( i=1; i<=10; i++ )); do
   for subdir in $(find $DIR -mindepth 1 -type d); do
     ZIP_FILE_PATH=$(find $subdir -iname \*.zip)
     ZIP_FILE_NAME=${ZIP_FILE_PATH##*/}
-    zip -s0 $ZIP_FILE_PATH --out $DIR/$ZIP_FILE_NAME
+    # zip -s0 caused some issues with some files so we used cat instead
+    ZIP_FILE_PATH_NO_EXT=${ZIP_FILE_PATH%.*}
+    cat $ZIP_FILE_PATH_NO_EXT.z0* $ZIP_FILE_PATH > $DIR/$ZIP_FILE_NAME
     unzip $DIR/$ZIP_FILE_NAME -d $DIR
     rm $DIR/$ZIP_FILE_NAME
     rm -rf $subdir
